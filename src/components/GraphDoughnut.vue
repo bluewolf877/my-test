d48f28345f3b3d76dd4270cc20b8a922016be7a1
bluewolf877 <bluewolf877gm@gmail.com>
2022-03-05 14:12:18 +0900

    チャート追加

---
+++ src/components/GraphDoughnut.vue
@@ -0,0 +1,30 @@
+// GraphDoughnut.vue
+<script>
+// ここでこのコンポーネントで使用するグラフの種類を定義する。今回はドーナツグラフなのでDoughnutとなる。
+import { Doughnut } from 'vue-chartjs'
+
+export default {
+  extends: Doughnut,
+  data() {
+    return {
+      datas: {
+        // 凡例とツールチップに表示するラベル
+        labels: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu'],
+        // 表示するデータ
+        datasets: [
+          {
+            data: [5, 10, 15, 20, 50],
+            backgroundColor: ['#f87979', '#aa4c8f', '#38b48b', '#006e54', '#89c3eb']
+          }
+        ]
+      },
+      options: {
+        responsive: true
+      }
+    }
+  },
+  mounted() {
+    this.renderChart(this.datas, this.options)
+  }
+}
+</script>
---
+++ src/views/Test3.vue
@@ -0,0 +1,58 @@
+// GraphDoughnut.vue
+<template>
+  <div>
+    <div class="relative">
+      <div class="doughnut-style">
+        <doughnut-half id="half" style="transform: rotate(270deg)"></doughnut-half>
+      </div>
+      <div class="title-style">
+        <p style="text-align: left">タイトル</p>
+      </div>
+      <div class="description-style">
+        <p style="text-align: center">ここに重ねる文字1</p>
+        <p style="text-align: left">ここに重ねる文字2</p>
+        <p style="text-align: left">ここに重ねる文字3</p>
+      </div>
+    </div>
+    <div class="doughnut-style">
+      <doughnut id="all"></doughnut>
+    </div>
+  </div>
+</template>
+
+<script>
+// ここでこのコンポーネントで使用するグラフの種類を定義する。今回はドーナツグラフなのでDoughnutとなる。
+import doughnut from '@/components/GraphDoughnut.vue'
+import doughnutHalf from '@/components/GraphDoughnutHalf.vue'
+
+export default {
+  name: 'Test3',
+  components: {
+    doughnut,
+    doughnutHalf
+  }
+}
+</script>
+<style>
+.doughnut-style {
+  height: 300px;
+  width: 300px;
+  padding: 20px;
+}
+.title-style {
+  width: 300px;
+  position: absolute;
+  top: 0px;
+}
+.description-style {
+  height: 100px;
+  width: 300px;
+  margin: 20px;
+  position: absolute;
+  bottom: 50px;
+}
+.relative {
+  position: relative;
+  width: 300px;
+}
+</style>
